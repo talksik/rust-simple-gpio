d30f9569b9f2502074cdcd05bdac5f994092cf91
talksik <patel.arjun50@gmail.com>
2023-05-28 21:11:44 -0700

    compiling

---
+++ target/arm-unknown-linux-gnueabi/release/deps/rust_simple_gpio-cb59d7c51916711e.d
@@ -0,0 +1,5 @@
+/target/arm-unknown-linux-gnueabi/release/deps/rust_simple_gpio-cb59d7c51916711e: src/main.rs
+
+/target/arm-unknown-linux-gnueabi/release/deps/rust_simple_gpio-cb59d7c51916711e.d: src/main.rs
+
+src/main.rs:
---
+++ target/arm-unknown-linux-gnueabi/release/rust-simple-gpio.d
@@ -0,0 +1,1 @@
+/target/arm-unknown-linux-gnueabi/release/rust-simple-gpio: /project/src/main.rs
