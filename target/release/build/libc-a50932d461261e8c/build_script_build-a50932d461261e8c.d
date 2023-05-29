6424fb031dffa568a874533128d0c16b3b2b6211
talksik <patel.arjun50@gmail.com>
2023-05-28 20:56:09 -0700

    update cross compile

---
+++ target/release/build/libc-a50932d461261e8c/build_script_build-a50932d461261e8c.d
@@ -0,0 +1,5 @@
+/target/release/build/libc-a50932d461261e8c/build_script_build-a50932d461261e8c: /cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.144/build.rs
+
+/target/release/build/libc-a50932d461261e8c/build_script_build-a50932d461261e8c.d: /cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.144/build.rs
+
+/cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.144/build.rs:
